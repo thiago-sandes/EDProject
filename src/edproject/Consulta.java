f164f7e66f77d38724e9a558f3f18010981893b6
thiago-sandes <thiago-sandes@hotmail.com>
2017-04-24 09:02:02 -0300

    Revisão de código.

--- src/edproject/Consulta.java
+++ src/edproject/Consulta.java
@@ -12,7 +12,7 @@ import javax.swing.JOptionPane;
  * 
  * @author Thiago Sandes e André Biriba
  * @version 1.0
- * @since entrega de projeto da materia de POO 18/04/2017
+ * @since entrega de projeto da materia Estrutura de Dados.
  */
 public class Consulta {
     private String nomeMedico;
